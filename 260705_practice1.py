a4da0a31cb2a7efe7f1cabea4a65dde1512b3f43
Takuto Wada <takutaku4ru8@gmail.com>
2026-07-05 21:41:04 +0900

    add practice1.py

---
+++ 260705_practice1.py
@@ -0,0 +1,10 @@
+name = "takuto"
+age = 27
+favorite_food = "sushi"
+
+print("こんにちは、私の名前は" + name + "です。"
+      "年齢は" + str(age) + "歳で、"
+      "好きな食べ物は" + favorite_food + "です。")
+
+name = input("名前を入力してください: ")
+print(name)
